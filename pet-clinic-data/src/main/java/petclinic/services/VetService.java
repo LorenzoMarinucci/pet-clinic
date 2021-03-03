add47f449118624528e942dc43ed67586ab03bf2
LorenzoMarinucci <lorenmarinucci@gmail.com>
2021-03-03 11:44:00 -0300

    Implement interfaces for services. Closes #4

---
+++ pet-clinic-data/src/main/java/petclinic/services/VetService.java
@@ -0,0 +1,15 @@
+package petclinic.services;
+
+import java.util.Set;
+
+import petclinic.model.Vet;
+
+public interface VetService {
+	
+	Vet findById(Long id);
+	
+	Vet save(Vet vet);
+	
+	Set<Vet> findaAll();
+
+}
