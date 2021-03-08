517b5dc26e332d1ba7c23d2616813d2bf0bd7ef9
LorenzoMarinucci <lorenmarinucci@gmail.com>
2021-03-08 11:26:59 -0300

    Added Visits, updated Owner with Pet relationship. Closes #25

--- pet-clinic-data/src/main/java/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/petclinic/model/PetType.java
@@ -2,4 +2,13 @@ package petclinic.model;
 
 public class PetType extends BaseEntity {
 
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
