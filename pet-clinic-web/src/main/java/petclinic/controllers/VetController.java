8cef4367bc99b24bdc39252bdb43d846002954a4
LorenzoMarinucci <lorenmarinucci@gmail.com>
2021-04-17 21:51:56 -0300

    Created vets JSON endpoint. Close #70

--- pet-clinic-web/src/main/java/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/petclinic/controllers/VetController.java
@@ -6,25 +6,34 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.ResponseBody;
+import petclinic.model.Vet;
 import petclinic.services.VetService;
 
+import java.util.Set;
+
 @Controller
-@RequestMapping({ "/vets", "/vets/ ", "/vets/index", "/vets/index.html", "/vets.html"})
 public class VetController {
 
-	private final VetService vetService;
+    private final VetService vetService;
 
-	@Autowired
-	public VetController(VetService vetService) {
-		this.vetService = vetService;
-	}
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
 
-	@GetMapping
-	public String listVets(Model model) {
+    @GetMapping({"/vets", "/vets/ ", "/vets/index", "/vets/index.html", "/vets.html"})
+    public String listVets(Model model) {
 
-		model.addAttribute("vets", vetService.findAll());
+        model.addAttribute("vets", vetService.findAll());
 
-		return "vets/index";
-	}
+        return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody
+    Set<Vet> getVetsJson() {
+        return vetService.findAll();
+    }
 
 }
